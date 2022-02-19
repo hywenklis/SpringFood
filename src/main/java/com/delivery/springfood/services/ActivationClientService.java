85471bd61649ec988795cd3bd062cae9adc2d0cb
hywenklis <hywenklis@hotmail.com>
2022-02-19 18:35:04 -0300

    Desambiguação de beans com @Primary

--- src/main/java/com/delivery/springfood/services/ActivationClientService.java
+++ src/main/java/com/delivery/springfood/services/ActivationClientService.java
@@ -4,19 +4,14 @@ import com.delivery.springfood.models.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class ActivationClientService {
 
     @Autowired
-    private List<Notifier> notifiers;
+    private Notifier notifier;
 
     public void activate(Client client) {
         client.activate();
-
-        for (Notifier notifier : notifiers) {
-            notifier.notify(client, "Seu cadastro no sistema está ativo!");
-        }
+        notifier.notify(client, "Seu cadastro no sistema está ativo!");
     }
 }
--- src/main/java/com/delivery/springfood/services/NotifierEmail.java
+++ src/main/java/com/delivery/springfood/services/NotifierEmail.java
@@ -1,8 +1,10 @@
 package com.delivery.springfood.services;
 
 import com.delivery.springfood.models.Client;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class NotifierEmail implements Notifier{
 
