913af4e89b521d88222eb1a6b0e0006ff3b446fb
hywenklis <hywenklis@hotmail.com>
2022-02-19 22:33:31 -0300

    Acessando propriedades com @ConfigurationProperties

--- src/main/java/com/delivery/springfood/services/NotifierEmail.java
+++ src/main/java/com/delivery/springfood/services/NotifierEmail.java
@@ -1,16 +1,25 @@
 package com.delivery.springfood.services;
 
+import com.delivery.springfood.config.NotifierProperties;
 import com.delivery.springfood.config.NotifierType;
 import com.delivery.springfood.enums.TypeUrgency;
 import com.delivery.springfood.models.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @NotifierType(TypeUrgency.URGENCY)
 @Component
 public class NotifierEmail implements Notifier{
 
+    @Autowired
+    private NotifierProperties notifierProperties;
+
     @Override
     public void notify(Client client, String message) {
+
+        System.out.println("Host: " + notifierProperties.getHostServer());
+        System.out.println("Porta: " + notifierProperties.getPortServer());
+
         System.out.printf("Notificando %s através do email %s: %s\n",
                 client.getName(), client.getEmail(), message);
     }
