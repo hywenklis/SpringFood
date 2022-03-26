4eec1b46ed049f4b9122d75aafe85e16ef9b5a5e
Hywenklis <hywenklis@hotmail.com>
2022-03-26 16:02:48 -0300

    Adicionando model de permissão

---
+++ src/main/java/com/delivery/springfood/domain/model/Permission.java
@@ -0,0 +1,28 @@
+package com.delivery.springfood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Permission {
+    
+    @Id
+    @Column(nullable = false)
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+}
