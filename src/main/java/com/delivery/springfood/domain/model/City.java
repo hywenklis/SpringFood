51774e5d18911a4596f3bbb16a234cc6d8817cd0
Hywenklis <hywenklis@hotmail.com>
2022-03-26 16:10:19 -0300

    Adicionando Estados e cidades com seus relacionamentos

---
+++ src/main/java/com/delivery/springfood/domain/model/City.java
@@ -0,0 +1,31 @@
+package com.delivery.springfood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class City {
+    
+    @Id
+    @Column(nullable = false)
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FederativeUnit federativeUnit;
+}
---
+++ src/main/java/com/delivery/springfood/domain/model/FederativeUnit.java
@@ -0,0 +1,25 @@
+package com.delivery.springfood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class FederativeUnit {
+    
+    @Id
+    @Column(nullable = false)
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
