426d234cb5a28370c5c122288f528fb04f482002
Hywenklis <hywenklis@hotmail.com>
2022-04-02 23:29:06 -0300

    Utilizando ResposeEntity para melhorar a manipulação dos http status

--- src/main/java/com/delivery/springfood/api/controller/FederativeUnitController.java
+++ src/main/java/com/delivery/springfood/api/controller/FederativeUnitController.java
@@ -3,7 +3,9 @@ package com.delivery.springfood.api.controller;
 import com.delivery.springfood.domain.model.FederativeUnit;
 import com.delivery.springfood.domain.repository.FederativeUnitRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,16 @@ public class FederativeUnitController {
     @GetMapping
     public List<FederativeUnit> list() {
         return federativeUnitRepository.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FederativeUnit> search(@PathVariable Long id) {
+
+        FederativeUnit federativeUnit = federativeUnitRepository.search(id);
+
+        if (federativeUnit != null) {
+            return ResponseEntity.ok(federativeUnit);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/delivery/springfood/api/controller/KitchenController.java
+++ src/main/java/com/delivery/springfood/api/controller/KitchenController.java
@@ -3,6 +3,7 @@ package com.delivery.springfood.api.controller;
 import com.delivery.springfood.domain.model.Kitchen;
 import com.delivery.springfood.domain.repository.KitchenRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,13 @@ public class KitchenController {
     }
 
     @GetMapping("/{id}")
-    public Kitchen search(@PathVariable Long id) {
-        return kitchenRepository.search(id);
+    public ResponseEntity<Kitchen> search(@PathVariable Long id) {
+
+        Kitchen kitchen = kitchenRepository.search(id);
+
+        if (kitchen != null) {
+            return ResponseEntity.ok(kitchen);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
